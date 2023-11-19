7bbd7ce1f92ba281e00b615cfc5dfc9e7c1a475a
Dunya Kirkali <dunya.kirkali@booking.com>
2023-11-19 20:55:05 +0100

    Add harpoon

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -73,6 +73,7 @@ require('lazy').setup({
   'tpope/vim-fugitive',
   'tpope/vim-rhubarb',
   'github/copilot.vim',
+  'ThePrimeagen/harpoon',
 
   -- Detect tabstop and shiftwidth automatically
   'tpope/vim-sleuth',
