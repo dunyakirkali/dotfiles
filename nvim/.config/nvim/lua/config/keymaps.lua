410e5b12ac8329ebd5fcdd6961ead643c4d986bc
Dunya Kirkali <dunya.kirkali@booking.com>
2023-12-01 19:32:06 +0100

    Add nvim config

---
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -0,0 +1,22 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
+local mark = require("harpoon.mark")
+local ui = require("harpoon.ui")
+
+vim.keymap.set("n", "<leader>a", mark.add_file, { desc = "Harpoon" })
+vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu, { desc = "Harpoons" })
+
+vim.keymap.set("n", "<C-h>", function()
+  ui.nav_file(1)
+end)
+vim.keymap.set("n", "<C-j>", function()
+  ui.nav_file(2)
+end)
+vim.keymap.set("n", "<C-k>", function()
+  ui.nav_file(3)
+end)
+vim.keymap.set("n", "<C-l>", function()
+  ui.nav_file(4)
+end)
---
+++ nvim/.config/nvim/lua/plugins/harpoon.lua
@@ -0,0 +1,9 @@
+return {
+  {
+    "ThePrimeagen/harpoon",
+    -- optional for floating window border decoration
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+  },
+}
