ca70431b2ed7c2209d68d49a1bd1ea0e3058f3a8
Dunya Kirkali <dunya.kirkali@booking.com>
2023-12-28 10:29:30 +0100

    Add keymaps for monorepo

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -30,3 +30,38 @@ local wk = require("which-key")
 wk.register({
 	["gww"] = { "gw", "Re-wrap" },
 })
+
+wk.register({
+	["<leader>m"] = { "<leader>m", "Monorepo" },
+})
+-- Monorepo
+
+vim.keymap.set("n", "<leader>ma", function()
+	require("monorepo").add_project()
+end, { desc = "Add Project" })
+
+vim.keymap.set("n", "<leader>mr", function()
+	require("monorepo").remove_project()
+end, { desc = "Remove Project" })
+
+vim.keymap.set("n", "<leader>mm", function()
+	require("telescope").extensions.monorepo.monorepo()
+end, { desc = "Monorepo" })
+
+vim.keymap.set("n", "<leader>mt", function()
+	require("monorepo").toggle_project()
+end, { desc = "Toggle Project" })
+
+for i = 1, 9 do
+	vim.keymap.set("n", "<leader>m" .. i, function()
+		require("monorepo").go_to_project(i)
+	end)
+end
+
+vim.keymap.set("n", "[m", function()
+	require("monorepo").previous_project()
+end, { desc = "Previous Project" })
+
+vim.keymap.set("n", "]m", function()
+	require("monorepo").next_project()
+end, { desc = "Next Project" })
