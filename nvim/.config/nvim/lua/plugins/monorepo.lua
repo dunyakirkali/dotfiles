c7e8ee216842a4e1e1ded5ff8ed68365fb5ed73d
Dunya Kirkali <dunya.kirkali@booking.com>
2023-12-28 10:29:22 +0100

    Enable notify for monorepo vim

--- nvim/.config/nvim/lua/plugins/monorepo.lua
+++ nvim/.config/nvim/lua/plugins/monorepo.lua
@@ -2,7 +2,7 @@ return {
 	"imNel/monorepo.nvim",
 	config = function()
 		require("monorepo").setup({
-			silent = true, -- Supresses vim.notify messages
+			silent = false, -- Supresses vim.notify messages
 			autoload_telescope = true, -- Automatically loads the telescope extension at setup
 			data_path = vim.fn.stdpath("data"), -- Path that monorepo.json gets saved to
 		})
