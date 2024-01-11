2e4206074162829d8df25ce737eb8925add52f24
Dunya Kirkali <dunyakirkali@gmail.com>
2024-01-11 21:26:41 +0100

    Move to harpoon2

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -2,21 +2,26 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-local mark = require("harpoon.mark")
-local ui = require("harpoon.ui")
+local harpoon = require("harpoon")
 
-vim.keymap.set("n", "<leader>a", mark.add_file, { desc = "Harpoon" })
-vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu, { desc = "Harpoons" })
+harpoon:setup()
+
+vim.keymap.set("n", "<leader>a", function()
+  harpoon:list():append()
+end)
+vim.keymap.set("n", "<C-e>", function()
+  harpoon.ui:toggle_quick_menu(harpoon:list())
+end)
 
 vim.keymap.set("n", "<C-h>", function()
-  ui.nav_file(1)
+  harpoon:list():select(1)
 end)
 vim.keymap.set("n", "<C-j>", function()
-  ui.nav_file(2)
+  harpoon:list():select(2)
 end)
 vim.keymap.set("n", "<C-k>", function()
-  ui.nav_file(3)
+  harpoon:list():select(3)
 end)
 vim.keymap.set("n", "<C-l>", function()
-  ui.nav_file(4)
+  harpoon:list():select(4)
 end)
--- nvim/.config/nvim/lua/plugins/harpoon.lua
+++ nvim/.config/nvim/lua/plugins/harpoon.lua
@@ -1,6 +1,7 @@
 return {
   {
     "ThePrimeagen/harpoon",
+    branch = "harpoon2",
     -- optional for floating window border decoration
     dependencies = {
       "nvim-lua/plenary.nvim",
